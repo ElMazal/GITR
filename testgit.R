39884a4e6d0c867da543a880489ca63b12a2f0b7
ElMazal <enmycb@umsystem.edu>
2024-07-06 13:33:40 -0500

    trying

---
+++ testgit.R
@@ -0,0 +1,20 @@
+1+1
+ellen <- 4
+ellen + ellen
+x <-ellen - 1
+x + 1 
+y <- (ellen)^2
+y
+ellen + ellen - 4
+y - ellen
+x -2
+chimwemwe <- 7 + ellen
+chimwemwe
+chikumbutso <- chimwemwe - 5
+chikumbutso
+chifuniro <- chikumbutso - 3
+chifuniro
+kids <- chimwemwe + chikumbutso + chifuniro
+kids
+jay = kids + ellen 
+jay
