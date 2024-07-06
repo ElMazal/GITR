fc9e1ffa7bc24e53f38d1d080020718f32528240
ElMazal <enmycb@umsystem.edu>
2024-07-06 13:18:07 -0500

    ten more line = 20

--- Testgitr.R
+++ Testgitr.R
@@ -8,4 +8,13 @@ y
 ellen + ellen - 4
 y - ellen
 x -2
-
+chimwemwe <- 7 + ellen
+chimwemwe
+chikumbutso <- chimwemwe - 5
+chikumbutso
+chifuniro <- chikumbutso - 3
+chifuniro
+kids <- chimwemwe + chikumbutso + chifuniro
+kids
+jay = kids + ellen 
+jay
